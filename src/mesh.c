be851a4ac89e4e0e7d9f4e5b0c74b335f5ea78bf
Bryance Oyang <bryance@ducky>
2023-04-10 04:52:50 -0700

    triangle3 deriv integral

--- src/mesh.c
+++ src/mesh.c
@@ -791,13 +791,28 @@ double triangle2_scalar_stress(struct vec *restrict c, struct element *restrict 
 /**
  * integrates
  */
-static double canon_triangle3_integral_grad(double *A, double *D)
+static double canon_triangle3_integral_grad(double *A, double *B)
 {
 	return
-	canon_triangle3.I0[0] * (A[0]*D[0] + A[1]*D[1]) +
-	canon_triangle3.I0[5] * (A[2]*D[2]) +
-	canon_triangle3.I0[1] * (A[0]*D[1] + A[1]*D[0]) +
-	canon_triangle3.I0[3] * ((A[0] + A[1])*D[2] + (D[0] + D[1])*A[2]);
+	canon_triangle3.I0[0] * (A[0]*B[0]) +
+	canon_triangle3.I0[0] * (A[0]*B[1] + A[1]*B[0]) +
+	canon_triangle3.I0[0] * (A[0]*B[2] + A[1]*B[1] + A[2]*B[0]) +
+	canon_triangle3.I0[0] * (A[1]*B[2] + A[2]*B[1]) +
+	canon_triangle3.I0[0] * (A[2]*B[2]) +
+
+	canon_triangle3.I0[0] * (A[0]*B[3] + A[3]*B[0]) +
+	canon_triangle3.I0[0] * (A[0]*B[4] + A[4]*B[0] + A[1]*B[3] + A[3]*B[1]) +
+	canon_triangle3.I0[0] * (A[3]*B[2] + A[2]*B[3] + A[1]*B[4] + A[4]*B[1]) +
+	canon_triangle3.I0[0] * (A[2]*B[4] + A[4]*B[2]) +
+
+	canon_triangle3.I0[0] * (A[3]*B[3] + A[0]*B[5] + A[5]*B[0]) +
+	canon_triangle3.I0[0] * (A[1]*B[5] + A[5]*B[1] + A[3]*B[4] + A[4]*B[3]) +
+	canon_triangle3.I0[0] * (A[4]*B[4] + A[2]*B[5] + A[5]*B[2]) +
+
+	canon_triangle3.I0[0] * (A[3]*B[5] + A[5]*B[3]) +
+	canon_triangle3.I0[0] * (A[4]*B[5] + A[5]*B[4]) +
+
+	canon_triangle3.I0[0] * (A[5]*B[5]);
 }
 
 static void canon_triangle3_acoeff()
